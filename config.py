1fb6da3c7fafe16de559ec165a31ef580b42bd3d
Axren-source <phengalexandre@gmail.com>
2026-04-07 16:36:04 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+API_KEY = "d059fd3f67454ac8baecbd4fd32c3bc2"
+BASE_URL = "https://api.football-data.org/v4"
+
+# 🌍 Competitions (clubs + international)
+COMPETITIONS = {
+    "WC": "FIFA World Cup",
+    "CL": "UEFA Champions League",
+    "BL1": "Bundesliga",
+    "DED": "Eredivisie",
+    "BSA": "Campeonato Brasileiro Série A",
+    "PD": "Primera Division",
+    "FL1": "Ligue 1",
+    "ELC": "Championship",
+    "PPL": "Primeira Liga",
+    "EC": "European Championship",
+    "SA": "Serie A",
+    "PL": "Premier League"
+}
+
+CLUB_COMPETITIONS = [
+    "CL", "BL1", "DED", "BSA", "PD", "FL1", "ELC", "PPL", "SA", "PL"
+]
+
+INTERNATIONAL_COMPETITIONS = [
+    "WC", "EC"
+]
