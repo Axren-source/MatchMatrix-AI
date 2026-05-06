f9a13bf946fe43a738f9740b55e730c661d786e9
Axren-source <phengalexandre@gmail.com>
2026-05-06 13:41:10 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://v3.football.api-sports.io/"
 
 HEADERS = {
-    "x-apisports-key": API_KEY
+    "x-apisports-key": "API_KEY"
 }
 
 # ✅ REAL API-FOOTBALL LEAGUE IDS
