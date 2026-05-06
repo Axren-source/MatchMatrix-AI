fd5ae849c074e7b56be2aeb361a1f831966d8f54
Axren-source <phengalexandre@gmail.com>
2026-05-06 18:39:33 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -2,44 +2,43 @@ import os
 
 API_KEY = os.getenv("API_KEY")
 
-BASE_URL = "https://v3.football.api-sports.io/"
+BASE_URL = "https://api.football-data.org/v4/"
 
 HEADERS = {
-    "x-apisports-key": API_KEY
+    "X-Auth-Token": API_KEY
 }
 
-# ✅ REAL API-FOOTBALL LEAGUE IDS
 COMPETITIONS = {
-    # 🌍 International
-    1: "FIFA World Cup",
-    4: "Euro Championship",
-
-    # 🇪🇺 Top Leagues
-    39: "Premier League",
-    140: "La Liga",
-    78: "Bundesliga",
-    135: "Serie A",
-    61: "Ligue 1",
-
-    # 🏆 Europe
-    2: "Champions League",
-    3: "Europa League",
-
-    # 🌎 Americas
-    71: "Brasileirão",
-    253: "MLS",
-
-    # 🌍 Africa
-    6: "African Cup of Nations"
+    "WC": "FIFA World Cup",
+    "CL": "UEFA Champions League",
+    "BL1": "Bundesliga",
+    "DED": "Eredivisie",
+    "BSA": "Brasileirão",
+    "PD": "La Liga",
+    "FL1": "Ligue 1",
+    "ELC": "Championship",
+    "PPL": "Primeira Liga",
+    "EC": "European Championship",
+    "SA": "Serie A",
+    "PL": "Premier League"
 }
 
-# ⚡ Fast queries (use these for /today)
-FAST_COMPETITIONS = [39, 140, 78, 135, 61, 2]
+CLUB_COMPETITIONS = [
+    "PL",
+    "PD",
+    "BL1",
+    "SA",
+    "FL1",
+    "CL"
+]
 
-# ⚽ Club matches
-CLUB_COMPETITIONS = [39, 140, 78, 135, 61, 2, 3]
+INTERNATIONAL_COMPETITIONS = [
+    "WC",
+    "EC"
+]
 
-# 🌍 International matches
-INTERNATIONAL_COMPETITIONS = [1, 4, 6]
-
-print(API_KEY)
+FAST_COMPETITIONS = [
+    "PL",
+    "PD",
+    "BL1"
+]
